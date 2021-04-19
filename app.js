0cba3fe2b67eb373b0ce6884f94e423d7393fc6b
phattaradanai-it <b6070632@g.sut.ac.th>
2021-04-19 11:04:31 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("หิวข้าวววววว")
+var firstname ="jojo";
+var lastname = "startos";
+
+var x = 10;
+{
+    var x = 12;
+    let y = 15;
+ 
+}
+console.log(x);
+
+var names = prompt("กรอกชื่อมาาา");
+alert(typeof(names));
+
